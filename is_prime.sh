6a7f52435a5fe1cb67a2fd779feb89dec02fe189
ekbalmalnas <ekbalhyd1990@gmail.com>
2018-02-28 16:15:34 +0530

    add somthing to code mr nene

---
+++ is_prime.sh
@@ -0,0 +1,27 @@
+#program to find prime no.or not
+
+isprime()
+{
+i=2
+con=$(( $num/2 ))
+while [[ $i -le $con ]] 
+do
+j=$(( $num % $i ))
+if [[ $j == 0 ]]
+then
+flag=2
+fi
+let i=i+1
+done
+}
+
+flag=0;
+num=$1
+isprime
+if [[ $flag == 2 ]]
+then 
+echo "$num is is not prime"
+else 
+echo "$num is prime no"
+fi
+
